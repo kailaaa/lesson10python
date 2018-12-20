d977be4df5063391b4263853031155b0e3641ece
kailaaa <44173171+kailaaa@users.noreply.github.com>
2018-12-20 13:24:53 -0500

    first commit

---
+++ lesson10/problem1.py
@@ -0,0 +1,9 @@
+print('-'*65)
+print('Life Decisoin Bot')
+print()
+answer = input('Are you dying? ')
+if answer == 'Yes' or answer == 'yes':
+	print('Ha, you suck!!!')
+else:
+	print('Damn, that sucks')
+print('-'*65)
